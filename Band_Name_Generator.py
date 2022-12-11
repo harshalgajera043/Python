3ed8cb69a54cc90ec033f579bc84fdfcf34e0f12
harshalgajera043 <83157209+harshalgajera043@users.noreply.github.com>
2022-12-11 22:56:31 +0530

    Add files via upload

---
+++ Band_Name_Generator.py
@@ -0,0 +1,5 @@
+#band name generator
+print("Welcome to the brand name generator!!")
+City_name = input("Which city do you live?\n")
+Pet_name=  input("what is the name of your pet?\n")
+print("Your band name can be" + " " + City_name + " " + Pet_name)
---
+++ Variables.py
@@ -0,0 +1,10 @@
+#variables interchange
+a = int(input("enter the value of a:"))
+b = int(input("enter the value of b:"))
+
+c = a
+a = b
+b = c
+
+print(a)
+print(b)
