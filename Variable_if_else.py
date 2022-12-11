e197abce169b49e8827cd4186ad656e7e824a338
harshalgajera043 <83157209+harshalgajera043@users.noreply.github.com>
2022-12-11 22:56:50 +0530

    Add files via upload

---
+++ Variable_if_else.py
@@ -0,0 +1,7 @@
+#python variables
+My_name = input("What is your name?")
+Confirmation = input(print("please confirm you name is" + " " + My_name))
+if Confirmation == "yes":
+  print("Hello " + My_name)
+else:
+  print(input("What name should be printed!!"))
