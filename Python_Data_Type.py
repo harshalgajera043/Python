c4bb152618717e47a19e2859553ed0ec6f13e0aa
harshalgajera043 <83157209+harshalgajera043@users.noreply.github.com>
2022-12-12 23:28:39 +0530

    Add files via upload

---
+++ Python_Data_Type.py
@@ -0,0 +1,21 @@
+#Data types
+#string
+Name = "Harshal"
+print(Name[1:6])
+
+#string concatination
+print("Har"+"shal")
+
+#integer
+Name1 = 1
+print(type(Name1))
+
+print(123+456)
+
+#float
+import math
+print(math.pi)
+
+#boolean
+True
+False
---
+++ Python_type_casting.py
@@ -0,0 +1,18 @@
+#Use "Ctrl+/" to add "#" and convert existing code into the comments
+#type casting
+name_entry = len(input("what is your name?"))
+
+#type_casting_to_string
+Name_entry = str(name_entry)
+print(type(Name_entry))
+print("your name has " + Name_entry + " charecters" )
+
+#type_casting_to_Boolean
+Name_entry_new = bool(name_entry)
+print(type(Name_entry_new))
+print(Name_entry_new)
+
+#type_casting_to_float
+New_Name_entry = float(name_entry)
+print(New_Name_entry)
+print(type(New_Name_entry))
