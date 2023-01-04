7e8810fbd2a6792751655724e7643cd4451a4066
harshalgajera043 <83157209+harshalgajera043@users.noreply.github.com>
2023-01-04 19:57:41 +0530

    Update Guess_me_Number_Guessing.py

--- Guess_me_Number_Guessing.py
+++ Guess_me_Number_Guessing.py
@@ -1,13 +1,4 @@
 #Number Guessing Game Objectives:
-
-# Include an ASCII art logo.
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
-
 #computer choice randomint
 import random
 from art import logo
@@ -47,8 +38,8 @@ def check_for_chance():
 keep_playing = True
 while keep_playing:
     print(logo)
-    computer_number = random.randint(0,100)
     print("I'm think about a number, between '0' and '100'.")
+    computer_number = random.randint(0,100)
     # print(f"computer choice is {computer_number}")
     # print("Welcome to number generator!!")
     # print(f"computer choice is {computer_number}")
