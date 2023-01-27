f4d842dd21e65c1f6799da72ba4da5107ffb0274
harshalgajera043 <83157209+harshalgajera043@users.noreply.github.com>
2023-01-27 12:09:09 +0530

    Add files via upload

---
+++ dict_comprehension.py
@@ -0,0 +1,9 @@
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+
+# Write your code below:
+word_list = sentence.split()
+result = {word:int(len(word)) for word in word_list}
+
+print(result)
+
